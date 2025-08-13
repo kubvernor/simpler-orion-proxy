29f024f28813ce492d93ffdd94b31dd89276109c
Dawid Nowak <nowakd@gmail.com>
2025-08-13 19:07:31 +0100

    Dockerizing

--- docker/start_proxy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#./replace_control_plane.sh
-#./orion --config orion-config.yaml
-
-echo "$@"
-#echo "Config file "
-export RUST_BACKTRACE=1
-#more /orion-config/orion-bootstrap.yaml
-./orion --config /orion-config/orion-bootstrap.yaml
